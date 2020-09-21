f0f6809f46ba34dd62f54037ac3068ce7b6ff408
bartek-krk <bartosz.lukasik@icloud.com>
2020-09-21 13:25:21 +0200

    final version

--- esp8266_firmware/esp8266_firmware.ino
+++ esp8266_firmware/esp8266_firmware.ino
@@ -1,27 +1,39 @@
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define TEMP_PIN 4
 
 const char* ssid = ""; //your wlan ssid
 const char* password = ""; //your wlan password
 String apiKey = ""; //your api key
-unsigned long delayMilliseconds(60000); //delay between requests
+unsigned long delayMilliseconds(3600000); //delay between requests (1 hour)
+
+OneWire oneWire(TEMP_PIN);
+DallasTemperature sensors(&oneWire);
 
 void setup() {
   Serial.begin(9600);
+  sensors.begin();
 }
 
 void loop() {
   Serial.println("Parsing JSON...");
   StaticJsonBuffer<200> jsonBuffer;
-  char json[] = "{\"stationId\":3,\"temperature\":22.53}";
+  
+  sensors.requestTemperatures();
+  float temperature = sensors.getTempCByIndex(0);
+  
+  String json = "{\"stationId\":3,\"temperature\":" + String(temperature) + "}";
+  
   JsonObject& root = jsonBuffer.parseObject(json);
   if(!root.success()) {
     Serial.println("parseObject() failed");
   } else {
     Serial.println("JSON parsing OK");
   }
-
 
   WiFi.begin(ssid, password);
 
@@ -30,9 +42,7 @@ void loop() {
     Serial.print("Connecting to WLAN... ");
   }
 
-
   String measurementsURL = "http://temperature-log.herokuapp.com/api/measurements/";
-
 
   HTTPClient http;
 
